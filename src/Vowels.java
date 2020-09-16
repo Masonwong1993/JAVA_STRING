eb51f1b467b82c3c65ad739b3f7f6dd62711177e
hjw <masonwong1993@gmail.com>
2020-09-16 17:21:28 -0500

    input from keyboard

--- src/Vowels.java
+++ src/Vowels.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Vowels {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a sentence :");
+        String sentence = sc.nextLine();
 
+        System.out.println(sentence);
+        System.out.println("Success!!!");
     }
 }
