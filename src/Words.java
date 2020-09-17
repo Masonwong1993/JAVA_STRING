4e4c3bd7bb3948f2c60ab127c235caae815fdecc
hjw <masonwong1993@gmail.com>
2020-09-16 20:45:59 -0500

    Q3 finished

--- src/Words.java
+++ src/Words.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Words {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a sentence :");
+        String sentence = sc.nextLine();
+        sentence = sentence.trim();
+        sentence = sentence.replace(",", " ");
+        String[] sentences = sentence.split(" ",0);
+        System.out.println(sentences.length);
+        int num = 0;
+        for (String s : sentences){
+            if (!s.equals("")) num += 1;
+
+        }
+
+        System.out.println("There are "+num+" words in this sentence");
+
+    }
+
 }
