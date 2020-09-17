23e56041941ed86885efbc8e387c42b1b3f7e9a5
hjw <masonwong1993@gmail.com>
2020-09-16 20:04:37 -0500

    Q2 finished

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a sentence :");
+        String sentence = sc.nextLine();
+        StringBuilder rev = new StringBuilder();
+        int len = sentence.length();
+        if (len !=0) {
+            for (int i = len - 1; i >= 0; i--) {
+                rev.append(sentence.charAt(i));
+            }
+        }
+        String s = rev.toString();
+        boolean ans = true;
+        if (!sentence.equals(s)) ans = false;
+        if (ans) System.out.println("The sentence is palindrome");
+        else System.out.println("The sentence is not palindrome");
+    }
 }
