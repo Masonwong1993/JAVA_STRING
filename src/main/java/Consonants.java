cd4359ccc5d345e1e94d774b822e4b23c03604a7
hjw <masonwong1993@gmail.com>
2020-09-22 15:19:36 -0500

    Consonants update

--- src/main/java/Consonants.java
+++ src/main/java/Consonants.java
@@ -34,4 +34,26 @@ public class Consonants {
         }
         return res.toString();
     }
+    public static int n(String s){
+        int num = 0;
+        List<Character> cons = new ArrayList<>();
+        cons.add('b');cons.add('m');cons.add('c');cons.add('d');cons.add('f');cons.add('g');cons.add('h');cons.add('j');cons.add('k');cons.add('l');
+        cons.add('n');cons.add('p');cons.add('q');cons.add('r');cons.add('s');cons.add('t');cons.add('v');cons.add('w');cons.add('x');cons.add('y');
+        cons.add('z');cons.add('B');cons.add('C');cons.add('D');cons.add('F');cons.add('G');cons.add('H');cons.add('J');cons.add('K');cons.add('L');
+        cons.add('M');cons.add('N');cons.add('P');cons.add('Q');cons.add('R');cons.add('S');cons.add('T');cons.add('V');cons.add('W');cons.add('X');
+        cons.add('Y');cons.add('Z');
+        StringBuilder res = new StringBuilder();
+        int len = s.length();
+        if (len!=0) {
+            for (int i = 0; i < len; i++) {
+                char c = s.charAt(i);
+                if (cons.contains(c)){
+                    res.append("*");
+                    num += 1;
+                }
+                else res.append(c);
+            }
+        }
+        return num;
+    }
 }
