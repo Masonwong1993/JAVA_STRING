ffd49c876d72ec7955484e43b9537b636a66018a
hjw <masonwong1993@gmail.com>
2020-09-22 14:19:11 -0500

    update Vowels

--- src/main/java/Vowels.java
+++ src/main/java/Vowels.java
@@ -8,16 +8,22 @@ public class Vowels {
         System.out.println("Please enter a sentence :");
         String sentence = sc.nextLine();
         int cnt = 0;
+        cnt = num(sentence,cnt);
+        System.out.println("The number of vowels is"+" "+cnt);
+
+    }
+    public static int num(String s, int cnt){
+
         List<Character> v = new ArrayList<>();
         v.add('a');v.add('e');v.add('u');v.add('i');v.add('o');v.add('A');v.add('E');v.add('U');v.add('I');v.add('O');
-        int len = sentence.length();
+        int len = s.length();
         if (len != 0){
             for (int i = 0; i < len;i++){
-                if (v.contains(sentence.charAt(i))){
+                if (v.contains(s.charAt(i))){
                     cnt++;
                 }
             }
         }
-        System.out.println("The number of vowels is"+" "+cnt);
+        return cnt;
     }
 }
